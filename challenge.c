a36be089e6e29aaf8bb4b5b740cec8c300c644ad
Victor Yu <159395423+vy038@users.noreply.github.com>
2025-08-27 01:32:08 -0400

    Update challenge.c

--- challenge.c
+++ challenge.c
@@ -43,11 +43,8 @@ int q2Array[Q2_ARRAY_SIZE];
 //-------------------------------------------------------------------------
 
 uint16_t q3(uint8_t x, uint8_t y) {
-    int leastBit = 0x01 & x;
-    int mostBit = (x >> 7) & 0x01;
-    x = x & 0b01111110;
-    x |= (mostBit << 0);
-    x |= (leastBit << 7);
+    x ^= 1 << 0;
+    x ^= 1 << 7;
     return ((uint16_t)x << 8) | y;
 }
 
